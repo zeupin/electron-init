ecfeb46961cce5448ffb7ea2ffd8f8084f4716dc
Macc Liu <maccliu@qq.com>
2019-07-20 02:01:58 +0800

    把init代码独立出来

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ const {app, Menu, BrowserWindow, ipcMain} = require('electron');
 let win;
 
 /**
- * app.on.ready
+ * 载入init页面，执行初始化代码
  */
-app.on('ready', () => {
+function init() {
   // 创建浏览器窗口并设置宽高
   win = new BrowserWindow({
     width: 400,
@@ -15,13 +15,20 @@ app.on('ready', () => {
     autoHideMenuBar: true,
   });
 
-  // 加载index页面
+  // 加载页面
   win.loadFile('src/pages/init/index.html');
 
-  // 添加window关闭触发事件
+  // 处理closed事件
   win.on('closed', () => {
     win = null; // 取消引用
   });
+}
+
+/**
+ * app.on.ready
+ */
+app.on('ready', () => {
+  init();
 });
 
 /**
