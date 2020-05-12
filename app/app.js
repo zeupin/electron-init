96ad9e626c461ec971933ed0155ccf3fbef18674
Macc Liu <maccliu@qq.com>
2020-05-12 11:18:21 +0800

    更新

--- app/app.js
+++ app/app.js
@@ -1,54 +1,45 @@
-const {app, Menu, BrowserWindow, ipcMain} = require('electron');
+const { app, BrowserWindow } = require('electron')
 
-let win;
+// 显式指定allowRendererProcessReuse，以免提示窗口出现警告信息
+app.allowRendererProcessReuse = true
 
-/**
- * 载入init页面，执行初始化代码
- */
-function init() {
-  // 创建浏览器窗口并设置宽高
-  win = new BrowserWindow({
-    width: 400,
-    height: 300,
-    maximizable: false,
-    minimizable: false,
-    autoHideMenuBar: true,
+function createWindow() {
+  // 创建浏览器窗口
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
     webPreferences: {
       // 不加这个，打开的页面将无法使用node.js。
       // 例如在使用require语句时，会报错"require() is not defined"。
-      nodeIntegration: true,
-    },
-  });
+      nodeIntegration: true
+    }
+  })
 
-  // 打开调试工具
-  win.webContents.openDevTools();
-
-  // 加载init页面
-  win.loadFile(`${__dirname}/pages/init/index.html`);
-
-  // 处理closed事件
-  win.on('closed', () => {
-    win = null; // 取消引用
-  });
+  // 并且为你的应用加载html
+  win.loadFile('app/pages/init/init.html')
 }
 
-/**
- * app.on.ready
- */
-app.on('ready', () => {
-  init();
-});
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// 部分 API 在 ready 事件触发后才能使用。
+app.whenReady().then(createWindow)
 
-/**
- * app.on.window-all-closed
- * 当所有的窗⼝口都被关闭时触发。
- */
+// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  app.quit();
-});
+  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+  // 否则绝大部分应用及其菜单栏会保持激活。
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
 
-/**
- * app.on.quit
- * 在应⽤用程序退出时触发。
- */
-app.on('quit', () => {});
+app.on('activate', () => {
+  // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+  // 通常在应用程序中重新创建一个窗口。
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. 也可以拆分成几个文件，然后用 require 导入。
